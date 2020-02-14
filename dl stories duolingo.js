807dafe4024529d1804c2c0f25d712358b19f427
elvper <jefdelaeu@gmail.com>
2020-02-14 21:59:38 +0100

    Add files via upload

--- dl stories duolingo.js
+++ dl stories duolingo.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Duolingo Stories Miner
-// @version      0.2.2
+// @version      0.2.3
 // @description  Collect stories and exercises from Duolingo
 // @author       somebody
 // @match        https://stories.duolingo.com/*
@@ -125,33 +125,42 @@ var learning = null,
 
 var output = "";
 var char_names = null;
+var ex_count = 0;
 
 var flattext = a => a.flatMap(a => a.text).join("");
 var flatStext = a => a.flatMap(a => flattext(a.syncedTexts)).join("");
 var flatperson = a => a.flatMap(a => a.person ? a.person : null);
 
 function dl_buttons() {
+	// button for each story
 	for (var set of [...document.getElementsByClassName("set")]) {
 		for (var story of [...set.getElementsByClassName("story")]) {
 			var button = document.createElement("button");
 			button.setAttribute("number", [...set.getElementsByClassName("story")].indexOf(story));
 			button.innerText = story.innerText;
-			button.addEventListener("click", function(){
-				request_story(this);
-			});
+			button.addEventListener("click", function(){request_story(this)});
 			set.append(button);
 		}
 	}
+	// button to get all
+	var button = document.createElement("button");
+	button.setAttribute("number", "all");
+	button.innerText = "Get all stories";
+	button.addEventListener("click", () => all_stories_get());
+	document.getElementsByClassName("stories-header")[0].append(button);
 }
 
-function construct(e, caller) {
+function construct(e, caller=null) {
 	console.log(JSON.stringify(e));
+	var exercises = collect_exercises(e);
 	// unique character names
 	char_names = new Set(flatperson(e.lines).filter(n => n));
 	// header
 	output = header[from_language + "_" + learning]() + br;
+	// story set and version
+	output += "> Set " + e.setNumber + "; CEFR " + e.cefrLevel + "; Story revision " + e.revision + "; Story length " + e.lines.length + "; Exercises " + ex_count + br;
 	// story title
-	output += "#### ! " + flatStext(e.lines[1].phrases) + br;
+	output += "#### ! " + flatStext(e.lines[1].phrases) + " (" + e.fromLanguageName + ")" + br;
 	// story text
 	for (var i = 2; i < e.lines.length; i++) {
 		// speaker
@@ -166,14 +175,18 @@ function construct(e, caller) {
 	// seperation between story text and exercises
 	output += b + bridge[from_language + "_" + learning]();
 	// exercises
-	output += collect_exercises(e);
+	output += exercises;
 	// remove previos if present
 	if (document.getElementById("output")) {
 		document.getElementById("output").parentElement.remove();
 		document.getElementById("close_output").remove();
 	}
 	// display output
-	display_output(caller);
+	if (caller){
+		display_output(caller);
+	} else {
+		document.getElementById("all_output").value += "---\n---\n---\n\n" + output;
+	}
 }
 
 function display_output(caller) {
@@ -201,10 +214,12 @@ function display_output(caller) {
 function collect_exercises(e) {
 	var ex = "", // output
 		a = []; // array of answer options
+	ex_count = 0;
 	for (var line of e.lines){
 		if (line.challenges.length){
-			console.log(line.challenges[0].type);
-			console.log(line.challenges[0]);
+			ex_count++;
+			//console.log(line.challenges[0].type);
+			//console.log(line.challenges[0]);
 			switch(line.challenges[0].type) {
 				case "multiple-choice":
 					// add question
@@ -263,14 +278,21 @@ function collect_exercises(e) {
 }
 
 function request_story(e) {
+	get_JSON(
+		"https://stories.duolingo.com/api/stories/" +  e.parentElement.getElementsByClassName("story")[e.getAttribute("number")].getAttribute("href").substr(9) + "?masterVersion=false",
+		construct,
+		e
+	);
+}
+
+function get_JSON(url, f, arg=null){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			construct(JSON.parse(xhttp.responseText), e);
+			f(JSON.parse(xhttp.responseText), arg);
 		}
 	};
-	xhttp.open("GET", "https://stories.duolingo.com/api/stories/" +  e.parentElement.getElementsByClassName("story")[e.getAttribute("number")].getAttribute("href").substr(9) + "?masterVersion=false"
-	, true);
+	xhttp.open("GET", url, true);
 	xhttp.send();
 }
 
@@ -283,25 +305,51 @@ function shuffleArray(array) {
     }
 }
 
-(function current_course() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-		   var user = JSON.parse(xhttp.responseText);
-		   learning = user.currentCourse.learningLanguage;
-		   from_language = user.currentCourse.fromLanguage;
+function all_stories_get(e){
+	//console.log(JSON.stringify(e));
+	// create display element
+	var div = document.createElement("div");
+	div.setAttribute("style", "padding-top: 10px;");
+	div.innerHTML = '<textarea id="all_output" rows="5" cols="75" style="border-width: 2px;border-color: darkred;border-style: solid;"></textarea>';
+	document.getElementsByClassName("stories-header")[0].append(div);
+	// get each story
+	var i = 1;
+	for (var set of [...document.getElementsByClassName("set")]) {
+		for (var story of [...set.getElementsByClassName("story")]) {
+			console.log(i++);
+			console.log(story.getAttribute("href").substr(9));
+			//get_JSON(
+			//	"https://stories.duolingo.com/api/stories/" +  story.getAttribute("href").substr(9) + "?masterVersion=false",
+			//	construct
+			//);
 		}
-	};
-	xhttp.open("GET", "https://stories.duolingo.com/api/user", true);
-	xhttp.send();
+	}
+}
+
+function request_sets() {
+	get_JSON(
+		"https://stories.duolingo.com/api2/stories?fromLanguage=" + from_language + "&learningLanguage=" + learning + "&masterVersions=false&illustrationFormat=png"
+	);
+}
+
+function set_language(user){
+	learning = user.currentCourse.learningLanguage;
+	from_language = user.currentCourse.fromLanguage;
+}
+
+(function current_course() {
+	get_JSON(
+		"https://stories.duolingo.com/api/user",
+		set_language
+	);
 })();
 
 // Wait for elements to load
 var transcript_load = setInterval(function() {
-   if (document.getElementsByClassName("story").length) {
+	if (document.getElementsByClassName("story").length) {
 		dl_buttons();
 		clearInterval(transcript_load);
-   }
+	}
 }, 100);
 
 })();
